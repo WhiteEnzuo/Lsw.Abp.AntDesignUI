42ef44e883be7ab9375838c7416245c50706cf79
enzuo <210327422@qq.com>
2023-11-07 21:19:30 +0800

    feat:support mysql

---
+++ src/BookStore.Blazor/BookStoreBlazorModule.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Net.Http;
+using Blazorise.Bootstrap5;
+using Blazorise.Icons.FontAwesome;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using BookStore.Blazor.Menus;
+using OpenIddict.Abstractions;
+using Volo.Abp.Autofac.WebAssembly;
+using Volo.Abp.AutoMapper;
+using Volo.Abp.Modularity;
+using Volo.Abp.UI.Navigation;
+using Lsw.Abp.AspnetCore.Components.Web.AntDesignTheme.Routing;
+using Lsw.Abp.AspnetCore.Components.Web.AntDesignTheme.Themes.AntDesignTheme;
+using Lsw.Abp.AspnetCore.Components.WebAssembly.AntDesignTheme;
+using Lsw.Abp.IdentityManagement.Blazor.WebAssembly.AntDesignUI;
+using Lsw.Abp.SettingManagement.Blazor.WebAssembly.AntDesignUI;
+using Lsw.Abp.TenantManagement.Blazor.WebAssembly.AntDesignUI;
+
+
+namespace BookStore.Blazor;
+
+[DependsOn(
+    typeof(AbpAutofacWebAssemblyModule),
+    typeof(BookStoreHttpApiClientModule),
+    typeof(AbpAspNetCoreComponentsWebAssemblyAntDesignThemeModule),
+    typeof(AbpAutoMapperModule),
+    typeof(AbpIdentityBlazorWebAssemblyAntDesignModule),
+    typeof(AbpTenantManagementBlazorWebAssemblyAntDesignModule),
+    typeof(AbpSettingManagementBlazorWebAssemblyAntDesignModule)
+)]
+public class BookStoreBlazorModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        var environment = context.Services.GetSingletonInstance<IWebAssemblyHostEnvironment>();
+        var builder = context.Services.GetSingletonInstance<WebAssemblyHostBuilder>();
+
+        ConfigureAuthentication(builder);
+        ConfigureHttpClient(context, environment);
+        ConfigureBlazorise(context);
+        ConfigureRouter(context);
+        ConfigureUI(builder);
+        ConfigureMenu(context);
+        ConfigureAutoMapper(context);
+    }
+
+    private void ConfigureRouter(ServiceConfigurationContext context)
+    {
+        Configure<AbpRouterOptions>(options =>
+        {
+            options.AppAssembly = typeof(BookStoreBlazorModule).Assembly;
+        });
+    }
+
+    private void ConfigureMenu(ServiceConfigurationContext context)
+    {
+        Configure<AbpNavigationOptions>(options =>
+        {
+            options.MenuContributors.Add(new BookStoreMenuContributor(context.Services.GetConfiguration()));
+        });
+    }
+
+    private void ConfigureBlazorise(ServiceConfigurationContext context)
+    {
+        context.Services
+            .AddBootstrap5Providers()
+            .AddFontAwesomeIcons();
+    }
+
+    private static void ConfigureAuthentication(WebAssemblyHostBuilder builder)
+    {
+        builder.Services.AddOidcAuthentication(options =>
+        {
+            builder.Configuration.Bind("AuthServer", options.ProviderOptions);
+            options.UserOptions.NameClaim = OpenIddictConstants.Claims.Name;
+            options.UserOptions.RoleClaim = OpenIddictConstants.Claims.Role;
+
+            options.ProviderOptions.DefaultScopes.Add("BookStore");
+            options.ProviderOptions.DefaultScopes.Add("roles");
+            options.ProviderOptions.DefaultScopes.Add("email");
+            options.ProviderOptions.DefaultScopes.Add("phone");
+        });
+    }
+
+    private static void ConfigureUI(WebAssemblyHostBuilder builder)
+    {
+        builder.RootComponents.Add<App>("#ApplicationContainer");
+
+    }
+
+    private static void ConfigureHttpClient(ServiceConfigurationContext context, IWebAssemblyHostEnvironment environment)
+    {
+        context.Services.AddTransient(sp => new HttpClient
+        {
+            BaseAddress = new Uri(environment.BaseAddress)
+        });
+    }
+
+    private void ConfigureAutoMapper(ServiceConfigurationContext context)
+    {
+        Configure<AbpAutoMapperOptions>(options =>
+        {
+            options.AddMaps<BookStoreBlazorModule>();
+        });
+    }
+}
