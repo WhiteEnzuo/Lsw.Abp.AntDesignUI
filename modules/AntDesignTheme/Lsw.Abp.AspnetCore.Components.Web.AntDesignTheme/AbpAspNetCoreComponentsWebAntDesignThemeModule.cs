428559c185df19699e4cc45a5350f6707f2d3ff4
enzuo <210327422@qq.com>
2023-11-07 21:21:32 +0800

    feat:support sqlServer

---
+++ modules/AntDesignTheme/Lsw.Abp.AspnetCore.Components.Web.AntDesignTheme/AbpAspNetCoreComponentsWebAntDesignThemeModule.cs
@@ -0,0 +1,17 @@
+﻿using Lsw.Abp.AntDesignUI;
+using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.Modularity;
+using Volo.Abp.UI.Navigation;
+
+namespace Lsw.Abp.AspnetCore.Components.Web.AntDesignTheme;
+
+[DependsOn(
+    typeof(AbpAntDesignUIModule),
+    typeof(AbpUiNavigationModule)
+)]
+public class AbpAspNetCoreComponentsWebAntDesignThemeModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+    }
+}
